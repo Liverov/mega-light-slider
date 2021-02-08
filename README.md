fdd3f4ec493fceb27dddabe3841a1a37239dcde9
Anton Liverov <liverov@yandex.ru>
2021-02-08 15:56:52 +0300

    Слайдер-карусель переведен на класс

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mega-light-slider
 Vanilla JavaScript very light slider with setinterval
+
+insert SLider.js class to you project and start
+
+const slider = new LightSlider('.slider'); // .slider - class name block of slides
--- slider.js
+++ slider.js
@@ -1,29 +1,60 @@
-'use strict';
+// Class style
+class LightSlider {
+  constructor(sliderClass) {
+    this._sliderClass = String(sliderClass);
+    this._sliderElement = document.querySelector(this._sliderClass);
+    this._nextBtn = document.querySelector('.next');
+    this._prevBtn = document.querySelector('.prev');
+  
+    this._clickHandler = this._clickHandler.bind(this);
+    this._nextBtn.addEventListener('click', this._clickHandler);
+    this._prevBtn.addEventListener('click', this._clickHandler);
+  
+  }
 
-const showSlides = 3;
-const widthSlides = 150;
-const runSetInterval = true;
-const timeout = 3000;
+  _nextSlide() {
+    this._sliderElement.appendChild(this._sliderElement.firstElementChild);
+  }
 
-const slider = document.querySelector('.slider');
-const next = document.querySelector('.next');
-const prev = document.querySelector('.prev');
+  _prevSlide() {
+    this._sliderElement.prepend(this._sliderElement.lastElementChild);
+  }
 
-let currentSlide = 0;
+  _clickHandler(e) {
+    e.preventDefault();
+    e.target.className === 'next' ? this._nextSlide() : this._prevSlide();
+  }
 
-document.querySelector('.wrapper').style.width = `${showSlides * widthSlides}px`;
-
-const handleClickNext = (e) => {
-  if(e) e.preventDefault();
-  slider.appendChild(slider.firstElementChild);
 }
 
-const handleClickPrev = (e) => {
-  if(e) e.preventDefault();
-  slider.prepend(slider.lastElementChild);
-}
+const slider = new LightSlider('.slider');
 
-if(runSetInterval) setInterval(handleClickNext, timeout);
 
-next.addEventListener('click', handleClickNext);
-prev.addEventListener('click', handleClickPrev);
+// Functional style
+// 'use strict';
+
+// const showSlides = 3;
+// const widthSlides = 150;
+// const runSetInterval = true;
+// const timeout = 3000;
+
+// const slider = document.querySelector('.slider');
+// const next = document.querySelector('.next');
+// const prev = document.querySelector('.prev');
+
+// document.querySelector('.wrapper').style.width = `${showSlides * widthSlides}px`;
+
+// const handleClickNext = (e) => {
+//   if(e) e.preventDefault();
+//   slider.appendChild(slider.firstElementChild);
+// }
+
+// const handleClickPrev = (e) => {
+//   if(e) e.preventDefault();
+//   slider.prepend(slider.lastElementChild);
+// }
+
+// if(runSetInterval) setInterval(handleClickNext, timeout);
+
+// next.addEventListener('click', handleClickNext);
+// prev.addEventListener('click', handleClickPrev);
